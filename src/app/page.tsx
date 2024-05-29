1bdc4d6877657fdc407efc1435de1d5bc6fd27b8
agora94 <gora.andrew@gmail.com>
2024-05-29 01:26:46 -0400

    map state & ref

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
-// import Map from "@/components/Map";
-import Spinner from '@/components/Spinner';
-import React, { Suspense } from 'react';
-const Map = React.lazy(() => import("@/components/Map"))
+import Map from "@/components/Map";
+import Spinner from "@/components/Spinner";
+import React from 'react';
 
 export default function Dashboard() {
   const resorts = [
@@ -9,12 +8,11 @@ export default function Dashboard() {
     {name: "Wachusett Mountain Ski Area", latitude: 42.502187, longitude: -71.886188},
     {name: "Killington Resort", latitude: 43.6269, longitude: -72.7968},
     {name: "Stowe Mountain Resort", latitude: 44.5344, longitude: -72.7804}
-  ]
+  ];
+
   return (
     <main className="flex min-h-max flex-col items-center justify-between p-24">
-      <Suspense fallback={<Spinner />}>
-        <Map resorts={resorts}></Map>
-      </Suspense>
+      <Map resorts={resorts}></Map>
     </main>
   );
 }
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -19,48 +19,60 @@ interface MapProps {
 
 const Map: React.FC<MapProps> = ({ resorts }) => {
   const mapContainerRef = useRef<HTMLDivElement>(null);
+  const mapRef = useRef<mapboxgl.Map | null>(null);
+  const [lng, setLng] = useState(resorts[0].longitude);
+  const [lat, setLat] = useState(resorts[0].latitude);
+  const [zoom, setZoom] = useState(5);
   const [loading, setLoading] = useState(true);
 
 
   useEffect(() => {
-    if (!mapContainerRef.current || resorts.length === 0) return;
+    if (mapRef.current || !mapContainerRef.current || resorts.length === 0) return;
 
-    const map = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [resorts[0].longitude, resorts[0].latitude],
-      zoom: 5,
-    });
+    // Instantiate a new Mapbox map
+      mapRef.current = new mapboxgl.Map({
+        container: mapContainerRef.current,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [lng, lat],
+        zoom: zoom,
+        attributionControl: false,
+      });
 
-    map.on('load', () => {
-      setLoading(false);
-      mapContainerRef.current?.classList.remove('invisible')
-    });
+      mapRef.current.on('move', () => {
+        setLng(parseFloat(mapRef.current!.getCenter().lng.toFixed(4)));
+        setLat(parseFloat(mapRef.current!.getCenter().lat.toFixed(4)));
+        setZoom(parseFloat(mapRef.current!.getZoom().toFixed(2)));
+      });
 
-    // Add navigation control (the +/- zoom buttons)
-    map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+      mapRef.current.on('load', (e) => {
+        setLoading(false);
+      });
 
-    // Add markers for each resort
-    resorts.forEach(resort => {
-      new mapboxgl.Marker()
-        .setLngLat([resort.longitude, resort.latitude])
-        .setPopup(
-          new mapboxgl.Popup({ offset: 25 }) // add popups
-            .setText(resort.name)
-        )
-        .addTo(map);
-    });
+      // Add navigation control (the +/- zoom buttons)
+      mapRef.current.addControl(new mapboxgl.NavigationControl(), 'top-right');
 
-    // Clean up on unmount
-    return () => map.remove();
-  }, [resorts]);
+      // Add markers for each resort
+      resorts.forEach(resort => {
+        new mapboxgl.Marker()
+          .setLngLat([resort.longitude, resort.latitude])
+          .setPopup(
+            new mapboxgl.Popup({ offset: 25 }) // add popups
+              .setText(resort.name)
+          )
+          .addTo(mapRef.current!);
+      });
+  });
 
   return (
     <>
+    {console.log('map component render')}
       {loading && (
         <Spinner />
       )}
-      <div className="map-container invisible overflow-hidden rounded-md" ref={mapContainerRef} style={{ height: '500px', width: '100%' }} ></div>
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div className="map-container overflow-hidden rounded-md" ref={mapContainerRef} style={{ height: '500px', width: '100%' }} ></div>
     </>
   );
 };
